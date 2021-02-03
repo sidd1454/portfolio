20b43ca42d014ee3710797c439da3faa6509c6dc
Siddharth Retiwala <sidd1454@gmail.com>
2021-02-03 15:04:25 -0600

    Removed alt for warnings

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,11 +4,7 @@ import image from "../image.jpg";
 export default function Home() {
   return (
     <main>
-      <img
-        src={image}
-        alt="Couples image"
-        className="absolute object-cover w-full h-full"
-      />
+      <img src={image} className="absolute object-cover w-full h-full" />
       <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
         <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">
           Aloha! We're Sid & Maitry.
